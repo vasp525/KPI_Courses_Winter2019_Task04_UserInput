45ab038dec7390b81bd3bf93f295e61478a3317c
Vadym Prykhodko <vadvasp@yahoo.com>
2019-02-18 09:22:49 +0200

    Добавил регулярку для фамилии, отчества.

--- src/controller/RegexContainer.java
+++ src/controller/RegexContainer.java
@@ -2,4 +2,7 @@ package controller;
 
 public interface RegexContainer {
     public static final String REGEX_NAME = "[А-Я][а-я]{1,20}";
+    public static final String REGEX_SURNAME = "[А-Я][а-я]{1,20}";
+    public static final String REGEX_MIDNAME = "[А-Я][а-я]{1,20}";
+
 }
