ad8f9b03423898ba4b25e9ca3a8a8c5c32fde84b
Vadym Prykhodko <vadvasp@yahoo.com>
2019-02-18 09:30:42 +0200

    Добавил регулярку на никнэйм.

--- src/controller/RegexContainer.java
+++ src/controller/RegexContainer.java
@@ -1,8 +1,9 @@
 package controller;
 
 public interface RegexContainer {
-    public static final String REGEX_NAME = "[А-Я][а-я]{1,20}";
-    public static final String REGEX_SURNAME = "[А-Я][а-я]{1,20}";
-    public static final String REGEX_MIDNAME = "[А-Я][а-я]{1,20}";
+    String REGEX_NAME = "[А-Я][а-я]{1,20}";
+    String REGEX_SURNAME = "[А-Я][а-я]{1,20}";
+    String REGEX_MIDNAME = "[А-Я][а-я]{1,20}";
+    String REGEX_NIKNAME = "[A-Za-z0-9]"; // need to be checked
 
 }
